4f968fcfee9c368210a7387d3ca8ddb5ba6692f3
unknown <edowney@AEPE-COL-1337.BSE.local>
2015-03-19 14:57:42 -0400

    Added Stuff.

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+var gulp = require('gulp'),
+	sass = require('gulp-sass'),
+	concat = require('gulp-concat');
+
+gulp.task('javascript', function () {
+	return gulp.src('app')
+		.pipe();
+});
+
+gulp.task('styles', function () {
+	return gulp.src('sass')
+		.pipe();
+});
